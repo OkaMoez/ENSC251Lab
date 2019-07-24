68b5d6edfc3ed539efe8e1a87078d4c046984d00
Brigham <brighamokano@gmail.com>
2019-07-24 12:55:46 -0700

    Lab 4 - Change Interface Comments to Past Tense in functions.hpp

--- functions.hpp
+++ functions.hpp
@@ -7,55 +7,55 @@ using namespace std;
 // User Input Function Prototypes
 void StartMenu(StudentList &D_list, StudentList &I_list);
     // Pre: Populated student lists
-    // Post: Begins program; no output
+    // Post: Program has been accessed
 
 void ListMenu(int kDomestic, StudentList &loaded_list);
     // Pre: One specific student list
-    // Post: Allows add, search, delete, and print; no output
+    // Post: User was able to add, search, delete, and/or print from/to student lists
 
 StudentList MergeList(StudentList &list1, StudentList &list2);
     // Pre: Two separate lists
-    // Post: One unified list
+    // Post: One new unified list
 
 void SearchType(int student_type, StudentList &loaded_list);
     // Pre: A student list and specified type (ie Domestic)
-    // Post: Calls Search(); no output
+    // Post: Call Search() which found and printed target students
 
 void Search(int search_type, StudentList &loaded_list);
     // Pre: A student list and specified type (ie Domestic) and call from SearchType()
-    // Post: Finds and prints target students; no return
+    // Post: Printed target students if found
 
 void MakeNewStudent(int student_type,StudentList &loaded_list);
     // Pre: A student list and specified type (ie Domestic)
-    // Post: Inserts new student into list; no return
+    // Post: Inserted new student into list
 
 void SearchAndDestroy(StudentList &loaded_list);
     // Pre: A student list
-    // Post: Deletes targets matching input from list; no return
+    // Post: Deleted targets from list who matched input
 
 string GetString(int target_type);
     // Pre: Desired string type (ie firstname)
-    // Post: A cleaned string of the specified type
+    // Post: Returned a correctly formated string of the specified type
 
 float GetCGPA();
     // Pre: None
-    // Post: A valid CGPA (ie float, 0 <= x <= 4.33)
+    // Post: Returned a valid CGPA (ie float, 0 <= x <= 4.33)
 
 int GetIntScore(int traget_type);
     // Pre: None
-    // Post: A valid research score (ie int, 0 <= x <= 100)
+    // Post: Returned a valid research score (ie int, 0 <= x <= 100)
 
 string CleanPNounInput(string dirty_string);
     // Pre: A string (assumed raw, ie " jo Hn ")
-    // Post: A string with no whitespace and a leading uppercase (ie "John")
+    // Post: Returned a string with no whitespace and a leading uppercase (ie "John")
 
 string CleanProvinceInput(string dirty_string);
     // Pre: A string (assumed raw, ie "bC")
-    // Post: An uppercase string
+    // Post: Returned an uppercase string
 
 string CleanCharacter(string string_in, char char_in);
     // Pre: A string (ie "happy") and a specific character (ie 'p')
-    // Post: The same string without instances of the specified character (ie "hay")
+    // Post: Returned the same string without instances of the specified character (ie "hay")
 
 
 
@@ -67,7 +67,7 @@ struct CompareStudent{ // Allows student comparison of a specified attribute.
     CompareStudent(int attribute);
     bool operator()( const Student &student1, const Student &student2);
     // Pre: Instance of CompareStudent with selected attribute (ie CompareStudent(kFirstname))
-    // Post: True if student1 sorts higher, false if student2 sorts higher
+    // Post: Returned true if student1 sorts higher, false if student2 sorts higher
 };
 
 
@@ -77,27 +77,27 @@ struct CompareStudent{ // Allows student comparison of a specified attribute.
 template <class T>
 int compare_firstname(const T& student1, const T& student2);
     // Pre: Two students
-    // Post: -1 if student1 sorts higher alphabetically, 0 if equal, 1 if lower
+    // Post: Returned -1 if student1 sorts higher alphabetically, 0 if equal, 1 if lower
 
 template <class T>
 int compare_lastname(const T& student1, const T& student2);
     // Pre: Two students
-    // Post: -1 if student1 sorts higher alphabetically, 0 if equal, 1 if lower
+    // Post: Returned -1 if student1 sorts higher alphabetically, 0 if equal, 1 if lower
 
 template <class T>
 int compare_cgpa(const T& student1, const T& student2);
     // Pre: Two students
-    // Post: 1 if student1 is greater, 0 if equal, 1 if less than student2
+    // Post: Returned 1 if student1 is greater, 0 if equal, 1 if less than student2
 
 template <class T>
 int compare_researchscore(const T& student1, const T& student2);
     // Pre: Two students
-    // Post: 1 if student1 is greater, 0 if equal, 1 if less than student2
+    // Post: Returned 1 if student1 is greater, 0 if equal, 1 if less than student2
 
 template <class T>
 int compare_location(const T& student1, const T& student2);
     // Pre: Two students
-    // Post: -1 if student1 sorts higher alphabetically, 0 if equal, 1 if lower
+    // Post: Returned -1 if student1 sorts higher alphabetically, 0 if equal, 1 if lower
     // Post cont: Overrides to -1 if student1 is domestic and student2 is international
 
 
@@ -105,10 +105,10 @@ int compare_location(const T& student1, const T& student2);
 // Std::Replacement Function Prototypes
 bool StringContains(string string_in, char char_in);
     // Pre: A string and a specified character
-    // Post: True if target character is found, false if not; replacing std::find()
+    // Post: Returned true if target character is found, false if not; replacing std::find()
 
 bool StringArrayContains(const string array_in[], string string_in);
     // Pre: A string array and a specified string
-    // Post: True if target string is found, false if not; replacing std::find()
+    // Post: Returned true if target string is found, false if not; replacing std::find()
 
 #endif // FUNCTIONS_HPP
